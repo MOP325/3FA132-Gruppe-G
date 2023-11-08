23c6ee26859778d0dcd5314af8df7a25f48e9a82
MOP325 <evdell@t-online.de>
2023-11-08 08:13:06 +0100

    8. 11. Downgrade

--- src/main/java/dev/hv/db/init/DbConnect.java
+++ src/main/java/dev/hv/db/init/DbConnect.java
@@ -2,28 +2,44 @@ package dev.hv.db.init;
 
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.statement.Update;
+
+import java.util.Properties;
+
 import org.jdbi.v3.core.Handle;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
 public class DbConnect implements IDbConnect {
+	
     private Jdbi jdbi;
     private DbConnect dbConnect;
+    private Properties dbProperties = new Properties();
 
     private String createCustomers = "CREATE TABLE Customers (Id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName VARCHAR, LastName VARCHAR)";
     private String createReading = "CREATE TABLE Reading (Id INTEGER PRIMARY KEY AUTOINCREMENT,cId INTEGER, MeterId INTEGER, DateOfReading VARCHAR, KindOfMeter VARCHAR, MeterCount DOUBLE,Substitute INTEGER DEFAULT 0, Comment VARCHAR,FOREIGN KEY (cId) REFERENCES Customer(Id))";
+    private String createUsers = "CREATE TABLE Users (Id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName VARCHAR, LastName VARCHAR, Password VARCHAR, Token VARCHAR)";
 
     private String dropCustomers = "DROP TABLE IF EXISTS Customers";
     private String dropReading = "DROP TABLE IF EXISTS Reading";
-
-    public void dBConnect(String uri, String user, String pw) {
-        // Initialize the Jdbi instance with the SQLite database connection
-        dbConnect.dBConnect("jdbc:sqlite:/Users/eliasdellingshausen/desktop/Hausverwaltung.db", "root", "root");
-
-        this.jdbi = Jdbi.create(uri, user, pw);
-    }
+    private String dropUsers = "DROP TABLE IF EXISTS Users";
 
     @Override
     public Jdbi getJdbi() {
+        loadProperties();
+        Jdbi jdbi = Jdbi.create(dbProperties.getProperty("db.url"));
         return jdbi;
+    }
+    
+    private void loadProperties() {
+    	try (InputStream input = new FileInputStream("src/config.txt"); ) {
+        	dbProperties.load(input);
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+    	}
+
     }
 
     @Override
@@ -33,38 +49,41 @@ public class DbConnect implements IDbConnect {
 
     @Override
     public void createAllTables() {
-        // Implement the logic to create tables using JDBI's @CreateSqlObject or other
-        // methods.
-        // This method is responsible for initializing your database schema.
 
         Handle handle = dbConnect.getJdbi().open();
         Update updateCustomer = null;
         Update updateReading = null;
+        Update updateUser = null;
         try {
+            handle.begin(); // Begin a transaction
+
             updateCustomer = handle.createUpdate(createCustomers);
             updateCustomer.execute();
 
             updateReading = handle.createUpdate(createReading);
             updateReading.execute();
-            // Add more createUpdate statements for other tables if needed
 
-            System.out.println("Tables created successfully.");
+            updateUser = handle.createUpdate(createUsers);
+            updateUser.execute();
+
+            handle.commit();
+            System.out.println("Datenbanktabellen wurden erfolgreich erstellt.");
         } catch (Exception e) {
+            handle.rollback();
             e.printStackTrace();
         } finally {
-            // Ensure that the Update object is closed in a finally block to release
-            // resources
             if (updateCustomer != null) {
                 updateCustomer.close();
             }
-
             if (updateReading != null) {
                 updateReading.close();
             }
 
-            if (handle == null) {
-                handle.close();
+            if (updateUser != null) {
+                updateUser.close();
             }
+
+            handle.close();
         }
     }
 
@@ -74,6 +93,8 @@ public class DbConnect implements IDbConnect {
         Handle handle = dbConnect.getJdbi().open();
         Update updateCustomer = null;
         Update updateReading = null;
+        Update updateUser = null;
+
         try {
             updateCustomer = handle.createUpdate(dropCustomers);
             updateCustomer.execute();
@@ -81,12 +102,13 @@ public class DbConnect implements IDbConnect {
             updateReading = handle.createUpdate(dropReading);
             updateReading.execute();
 
+            updateUser = handle.createUpdate(dropUsers);
+            updateReading.execute();
+
             System.out.println("Tables removed successfully.");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // Ensure that the Update objects are closed in a finally block to release
-            // resources
             if (updateCustomer != null) {
                 updateCustomer.close();
             }
@@ -94,12 +116,11 @@ public class DbConnect implements IDbConnect {
                 updateReading.close();
             }
 
-            // Ensure that the Handle is closed as well
-            if (handle != null) {
-                handle.close();
+            if (updateUser != null) {
+                updateUser.close();
             }
-        }
 
-        // Implement the logic to remove all tables.
+            handle.close();
+        }
     }
 }
