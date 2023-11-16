7ec9a821a189417631fe2a3d037e512b816de8ee
Jeremy Rentschler <jeremy.rentschler@cyancor.com>
2023-11-16 09:58:24 +0100

    Improce the code of the tests

--- src/test/java/dbConnectTest.java
+++ src/test/java/dbConnectTest.java
@@ -2,7 +2,7 @@ public class dbConnectTest {
 
     public static void main(String[] args) {
 
-        DbConnectJunit junitTest = new DbConnectJunit();
+        DbConnectJunitTest junitTest = new DbConnectJunitTest();
         CustomerTest customerTest = new CustomerTest();
         UserTest userTest = new UserTest();
         ReadingTest readingTest = new ReadingTest();
@@ -37,28 +37,5 @@ public class dbConnectTest {
 
         junitTest.teardown();
         junitTest.testRemoveAllTables();
-
-        // String sql = "SELECT Id, FirstName, LastName FROM Customer";
-
-        // DbConnect dbConnect = new DbConnect();
-        // dbConnect.dBConnect("jdbc:sqlite:/Users/eliasdellingshausen/desktop/Hausverwaltung.db",
-        // "root", "root");
-
-        // Handle handle = dbConnect.getJdbi().open();
-
-        // List<Customer> customers = handle.createQuery(sql).mapToBean(Customer.class)
-        // .list();
-
-        // handle.close();
-
-        // for (Customer customer : customers) {
-        // // Access and process customer data
-        // System.out.println("ID: " + customer.getId() + ", FirstName: " +
-        // customer.getFirstName() + ", LastName: "
-        // + customer.getLastName());
-        // }
-
     }
 }
-
-// org.jdbi.v3.core.statement.UnableToCreateStatementException: org.sqlite.SQLiteException: [SQLITE_ERROR] SQL error or missing database (table Users already exists) [statement:"CREATE TABLE Users (Id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName VARCHAR, LastName VARCHAR, Password VARCHAR, Token VARCHAR)", arguments:{positional:{}, named:{}, finder:[]}
