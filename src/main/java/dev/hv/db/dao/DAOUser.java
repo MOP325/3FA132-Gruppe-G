1b5c7ef45684886afc52606a1212dfe078e6ac54
MOP325 <evdell@t-online.de>
2024-02-28 10:39:08 +0100

    rest endpoints + web project init

--- src/main/java/dev/hv/db/dao/DAOUser.java
+++ src/main/java/dev/hv/db/dao/DAOUser.java
@@ -1,27 +1,25 @@
 package dev.hv.db.dao;
 
+import java.util.List;
+
 import org.jdbi.v3.core.Jdbi;
 
 import dev.hv.db.model.User;
 
-import java.util.List;
-
-public class DAOUser {
+class UserDAO {
 
     private Jdbi jdbi;
 
-    public DAOUser() {
-        jdbi = Jdbi.create("jdbc:your_database_url", "username", "password");
+    public UserDAO(Jdbi jdbi) {
+        this.jdbi = jdbi;
     }
 
     public void insert(User user) {
-        jdbi.useExtension(UserDAO.class,
-                dao -> dao.insert(user.getFirstName(), user.getLastName(), user.getPassword(), user.getToken()));
+        jdbi.useExtension(UserDAO.class, dao -> dao.insert(user.getFirstName(), user.getLastName(), user.getPassword(), user.getToken()));
     }
 
     public void update(User user) {
-        jdbi.useExtension(UserDAO.class, dao -> dao.update(user.getId(), user.getFirstName(), user.getLastName(),
-                user.getPassword(), user.getToken()));
+        jdbi.useExtension(UserDAO.class, dao -> dao.update(user.getId(), user.getFirstName(), user.getLastName(), user.getPassword(), user.getToken()));
     }
 
     public User findById(int id) {
@@ -34,5 +32,9 @@ public class DAOUser {
 
     public void delete(int id) {
         jdbi.useExtension(UserDAO.class, dao -> dao.delete(id));
+    }
+
+    public static UserDAO getInstance() {
+        return new UserDAO(jdbi.onDemand(UserDAO.class));
     }
 }
